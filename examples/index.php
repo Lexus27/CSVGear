f19b8224719fd7c3dcd9d07eeb483d4673c59507
Alexey <Lexus.1995@mail.ru>
2015-01-20 22:05:54 +1100

    Create index.php

---
+++ examples/index.php
@@ -0,0 +1,31 @@
+<?php
+namespace CSVGear;
+
+require_once dirname(__DIR__) . '/csv/csvgear.php';
+
+
+$csv = new CSV([
+	'index'
+],[
+	'items' => [
+
+		[
+			'src' 		=> 'result.csv',
+			'type'		=> 'file',
+			'config'	=> [
+				'charset'		=> 'Windows1251',
+				'offset'		=> 0,
+				'schema' 		=> [ 'contributor', 'observe', 'index', 'file' ],
+				'aliases'		=> [
+					'index' => function($key,$row) {
+						return implode(' - ', $row);
+
+					}
+				]
+			]
+		]
+
+	]
+]);
+
+$csv->flush('csv2.csv',true);
